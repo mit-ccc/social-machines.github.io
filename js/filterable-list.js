c901c82c4799f725e6badbe509063a456867f226
Peter Beshai <peter.beshai@gmail.com>
2017-11-07 10:57:21 -0500

    Improve documentation in filtering code

--- js/filterable-list.js
+++ js/filterable-list.js
@@ -10,7 +10,7 @@
  *
  *   npm install -g buble uglify-js
  *
- * Note that data is of the form:
+ * Note that `data` is of the form:
  * [
  *   {
        header: "Some Header",
@@ -21,6 +21,8 @@
      },
  *   ...
  * ]
+ *
+ * See publications.html to see publications.html for example usage.
  */
 function createFilterableList({
   rootSelector,
@@ -34,18 +36,22 @@ function createFilterableList({
   /** filter state **/
   let inputString = '';
   let activeTags = [];
-  let filteredData;
+  let filteredFlatData;
   let groupedFilteredData;
+  // for convenience, create a fully flat list of the data
   const flatData = data.reduce((accum, entry) => accum.concat(entry.items), []);
 
+  // find the list of tags from the data
+  const tags = discoverTagsFromData(flatData);
+
+  // look into query parameters to populate initial state
   readStateFromUrl();
 
   /** initialize **/
-
   const root = d3.select(rootSelector);
 
+  // add in the keyword search filter
   const inputContainer = root.append('div').attr('class', 'ui input');
-
   const input = inputContainer
     .append('input')
     .attr('type', 'text')
@@ -56,14 +62,14 @@ function createFilterableList({
       changeInputValue(this.value);
     });
 
+  // add in the tag filter
   const tagList = root.append('div').attr('class', 'filter-tags');
   tagList
     .append('div')
     .attr('class', 'filter-label')
     .text('Filter by Tag');
 
-  const tags = discoverTagsFromData(data);
-
+  // for each tag, create a pill to represent it
   tagList
     .selectAll('a')
     .data(tags)
@@ -75,14 +81,17 @@ function createFilterableList({
     .classed('teal', d => activeTags.includes(d))
     .text(d => d);
 
+  // add a message to be shown when no items match the filters
   const emptyMessage = root
     .append('p')
     .attr('class', 'empty-message')
     .text('No publications match the current filters.')
     .style('display', 'none');
 
+  // the parent for where the grouped filtered lists will be stored
   const filteredList = root.append('div').attr('class', 'filtered-list');
 
+  // filter and render the data
   update();
 
   function update() {
@@ -95,18 +104,35 @@ function createFilterableList({
   }
 
   /** render helpers **/
-
+  // re-render everything
   function render() {
     renderTags();
-    // renderFilteredList();
     renderAllGroups();
   }
 
+  // render the tags. Just has to update the counts and whether
+  // or not they are active. No enter/exit/update required since
+  // the list of tags never changes.
+  function renderTags() {
+    const countByTag = filteredCountByTag();
+    tagList
+      .selectAll('.filter-tag-item')
+      .classed('active', d => activeTags.includes(d))
+      .classed('teal', d => activeTags.includes(d))
+      .html(d => `${d} <span class="tag-count">${countByTag[d] || 0}</span>`);
+  }
+
+  // render the filtered grouped lists
   function renderAllGroups() {
+    // for each super group (e.g. Academic, Theses, Non-Academic, etc.)
     const binding = filteredList
       .selectAll('.filtered-list-super-group')
       .data(groupedFilteredData, d => d.id);
+
+    // remove those that are no longer matching the filters
     binding.exit().remove();
+
+    // add in ones that were not previously matched or rendered
     const entering = binding
       .enter()
       .append('div')
@@ -118,9 +144,10 @@ function createFilterableList({
       .attr('id', d => d.id)
       .text(d => d.header);
 
+    // create node where grouped items will live (e.g. papers by year)
     entering.append('div').attr('class', 'filtered-list-super-group-items');
 
-    // render the lists
+    // render the grouped item lists for each super group
     entering
       .merge(binding)
       .select('.filtered-list-super-group-items')
@@ -129,6 +156,8 @@ function createFilterableList({
         renderGroupedFilteredList(d.groupedItems, groupRoot, d);
       });
 
+    // if there are any items that match the filters, hide the empty message,
+    // otherwise show it.
     if (groupedFilteredData.length) {
       emptyMessage.style('display', 'none');
     } else {
@@ -136,42 +165,37 @@ function createFilterableList({
     }
   }
 
-  function renderTags() {
-    const countByTag = filteredCountByTag();
-    tagList
-      .selectAll('.filter-tag-item')
-      .classed('active', d => activeTags.includes(d))
-      .classed('teal', d => activeTags.includes(d))
-      .html(d => `${d} <span class="tag-count">${countByTag[d] || 0}</span>`);
-  }
+  /**
+   * Render a grouped list e.g. a collection of Academic papers that are
+   * grouped by year.
+   *
+   * @param {Object} groupData The entry of group data { key, values }
 
-  // function renderFilteredList() {
-  //   const binding = filteredList
-  //     .selectAll('.filtered-list-item')
-  //     .data(filteredData, d => d.id);
-  //   binding.exit().remove();
-  //   binding
-  //     .enter()
-  //     .append('li')
-  //     .attr('class', 'filtered-list-item')
-  //     .merge(binding)
-  //     .html(renderItem);
-  // }
-
+   */
   function renderGroupedFilteredList(groupData, root, groupEntry) {
+    // for each group within groupData, bind the data.
+    // e.g. for each year
     const binding = root
       .selectAll('.filtered-list-group')
       .data(groupData, d => d.key);
+
+    // remove those no longer matched by filters
     binding.exit().remove();
+
+    // add those newly matched
     const entering = binding
       .enter()
       .append('div')
       .attr('class', 'filtered-list-group');
 
+    // create object for overriding keys to be user specified labels
     const groupByLabels = Object.assign(
       { __null: 'Other' },
       groupEntry.groupByLabels
     );
+
+    // for each new group, add a header using the specified label
+    // or the key if not provided.
     entering
       .append('h3')
       .attr('class', 'ui header')
@@ -183,6 +207,7 @@ function createFilterableList({
         return label;
       });
 
+    // add a list for the papers within a group, adding in user-specified class
     const enteringLists = entering
       .append('ul')
       .attr(
@@ -190,7 +215,7 @@ function createFilterableList({
         'filtered-list-group-list ' + (listClass ? listClass : '')
       );
 
-    // render the lists
+    // render the lists for each group
     const itemBinding = entering
       .merge(binding)
       .select('ul')
@@ -202,10 +227,12 @@ function createFilterableList({
       .append('li')
       .attr('class', 'filtered-list-item')
       .merge(itemBinding)
-      .html(renderItem);
+      .html(renderItem); // use the user-specified renderItem function
   }
 
   /** URL encoding and decoding**/
+
+  // write state to query parameters for tags and keyword search
   function writeStateToUrl() {
     const encodedInputString = inputString.length
       ? encodeURIComponent(inputString)
@@ -222,6 +249,7 @@ function createFilterableList({
     window.history.replaceState({ encodedInputString, encodedTags }, '', url);
   }
 
+  // read in state from query parameters for tags and keyword search
   function readStateFromUrl() {
     const queryParams = getUrlQueryParams();
 
@@ -236,7 +264,7 @@ function createFilterableList({
   /** data processing **/
   // create a map from tag to count of filtered items
   function filteredCountByTag() {
-    return filteredData.reduce((accum, item) => {
+    return filteredFlatData.reduce((accum, item) => {
       if (item[tagKey]) {
         item[tagKey].forEach(tag => {
           if (accum[tag] == null) {
@@ -250,7 +278,8 @@ function createFilterableList({
     }, {});
   }
 
-  function discoverTagsFromData() {
+  // find all tags in the data and sort them
+  function discoverTagsFromData(flatData) {
     const tagMap = {};
     flatData.forEach(d => {
       const tagValue = d[tagKey];
@@ -268,32 +297,36 @@ function createFilterableList({
     return Object.keys(tagMap).sort();
   }
 
-  let allGroupedFilteredData;
-
+  // filter the data based on selected tags and keyword search
   function filterData() {
-    // filter based on tag
-    filteredData = flatData
+    // filter based on tag then keyword search in the flat list
+    filteredFlatData = flatData
       .filter(itemMatchesActiveTags)
       .filter(itemMatchesInputString);
 
+    // filter based on tag then keyword search for each of the groups
     groupedFilteredData = data
       .map(entry => {
         const groupFilteredData = entry.items
           .filter(itemMatchesActiveTags)
           .filter(itemMatchesInputString);
 
+        // allow individual entries to specify their own groupBy field
         const entryGroupBy = entry.groupBy || groupBy;
 
+        // group the items by the groupBy field and sort based on key.
         const groupedItems = d3
           .nest()
           .key(d => (d[entryGroupBy] == null ? '__null' : d[entryGroupBy]))
           .entries(groupFilteredData)
           .sort((a, b) => b.key - a.key);
 
+        // expand the entry to have groupedItems in it (non destructively)
         return Object.assign({}, entry, {
           groupedItems,
         });
       })
+      // remove groups with no items matching the filters
       .filter(groupedData => groupedData.groupedItems.length > 0);
   }
 
@@ -317,6 +350,8 @@ function createFilterableList({
     return hasAllTags;
   }
 
+  // true if the item matches the input string in any of its properites
+  // excluding html and id. Ignores case.
   function itemMatchesInputString(item) {
     if (!inputString.trim().length) {
       return true;
@@ -337,6 +372,7 @@ function createFilterableList({
 
   /** handlers **/
 
+  // toggle a tag as active or not
   function toggleFilterTag(tag) {
     const tagIndex = activeTags.indexOf(tag);
     if (tagIndex !== -1) {
@@ -347,17 +383,22 @@ function createFilterableList({
       activeTags.push(tag);
     }
 
+    // re-render the UI
     update();
   }
 
   function changeInputValue(value) {
     inputString = value;
 
+    // re-render the UI
     update();
   }
 }
 
-function clearData(rootSelector) {
+// top level function to clear a container. this is useful since
+// we want the server to render contents for better SEO, then we
+// clear it and have the JS re-render.
+function clearContents(rootSelector) {
   d3.select(rootSelector).html('');
 }
 
